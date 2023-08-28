9692d26fe3373b09e6fe5c5f035b60a66de56f46
CodistBae <kurtbae97@gmail.com>
2023-08-28 15:09:32 +0900

    하이요추가
    하이요

--- Fourcus/src/main/java/test.java
+++ Fourcus/src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("dfdfdf");
         System.out.println("네");
+        System.out.println("하이요");
     }
 }
