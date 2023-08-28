3ba04b81f3fd100e1085322365c2430e163a4596
hyeeun <hyeun9313@gmail.com>
2023-08-28 15:05:27 +0900

    로그인 구현
    아이디 비밀번호를 받아 ㅁ비교

--- Fourcus/src/main/java/test.java
+++ Fourcus/src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("dfdfdf");
-        System.out.println("안녕하세요");
+        System.out.println("네");
     }
 }
