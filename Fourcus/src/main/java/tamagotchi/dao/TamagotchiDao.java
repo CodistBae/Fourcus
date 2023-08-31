0503e5bbbe9d14300e68acb5aad9ca0404e30add
CodistBae <kurtbae97@gmail.com>
2023-08-31 11:00:03 +0900

    타마고치, 칭호 완

--- Fourcus/src/main/java/tamagotchi/dao/TamagotchiDao.java
+++ Fourcus/src/main/java/tamagotchi/dao/TamagotchiDao.java
@@ -3,8 +3,11 @@ package tamagotchi.dao;
 import common.DbUtils;
 import tamagotchi.vo.Tamagotchi;
 import tamagotchi.vo.Type;
+import title.vo.Title;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TamagotchiDao {
 
@@ -15,16 +18,17 @@ public class TamagotchiDao {
     }
 
     public void insert(Tamagotchi tamagotchi) {
-        String sql = "insert into Tamagotchi (Member_id, Tamagotchi_name, Level, Type) values ?, ?, ?";
+        String sql = "insert into Tamagotchi (Member_id, Tamagotchi_name, Level, Type) values (?,?, ?, ?)";
         try (Connection connection = dbUtils.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql)) {
 
-            ps.setString(1, tamagotchi.getTamagotchiName());
-            ps.setInt(2, tamagotchi.getLevel());
-            ps.setString(3, tamagotchi.getType().getName());
+            ps.setLong(1, tamagotchi.getMemberId());
+            ps.setString(2, tamagotchi.getTamagotchiName());
+            ps.setInt(3, tamagotchi.getLevel());
+            ps.setString(4, tamagotchi.getType().getName());
             ps.executeUpdate();
 
-            System.out.println("Tamagotchi append success.");
+            System.out.println("Tamagotchi insert success.");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -89,5 +93,32 @@ public class TamagotchiDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Long> getTotalStudyTime(long memberId) {
+        List<Long> timeList = new ArrayList<>();
+
+        String sql = """
+                select Cumulative_time from StudyHour
+                where Subject_id in (select id from Subject where Member_id = ?);
+                """;
+
+        try (Connection connection = dbUtils.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setLong(1, memberId);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                timeList.add(resultSet.getLong(1));
+            }
+
+            System.out.println("getTimeList success.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return timeList;
     }
 }
