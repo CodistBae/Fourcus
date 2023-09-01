cc02908f04e133ccd63dfa6e105fccec2e5ef044
chaewoniiii <102212394+chaewoniiii@users.noreply.github.com>
2023-09-01 19:02:57 +0900

    update MemberService.java delMember

--- Fourcus/src/main/java/member/service/MemberService.java
+++ Fourcus/src/main/java/member/service/MemberService.java
@@ -1,5 +1,9 @@
 package member.service;
 
+import group.dao.GroupDao;
+import group.service.GroupService;
+import groupmember.dao.GroupMemberDao;
+import groupmember.vo.GroupMember;
 import member.dao.MemberDao;
 import member.vo.Member;
 import tamagotchi.dao.TamagotchiDao;
@@ -12,12 +16,16 @@ import java.io.IOException;
 
 public class MemberService {
     private final TamagotchiDao tamagotchiDao;
-    private MemberDao dao;
+    private MemberDao mdao;
+    private GroupDao gdao;
+    private GroupMemberDao gmdao;
     public static Long loginId;
     public TitleService titleService;
 
     public MemberService() {
-        dao = MemberDao.getInstance();
+        mdao = MemberDao.getInstance();
+        gdao = GroupDao.getInstance();
+        gmdao = GroupMemberDao.getInstance();
         loginId = null;
         this.tamagotchiDao = TamagotchiDao.getInstance();
         this.titleService = TitleService.getInstance();
@@ -33,7 +41,7 @@ public class MemberService {
             do {
                 System.out.print("ID: ");
                 username = br.readLine();
-                m = dao.select(1, username);
+                m = mdao.select(1, username);
             } while (m != null);
             System.out.print("PASSWORD: ");
             String password = br.readLine();
@@ -41,7 +49,7 @@ public class MemberService {
             do {
                 System.out.print("닉네임: ");
                 nickname = br.readLine();
-                m = dao.select(2, nickname);
+                m = mdao.select(2, nickname);
             } while (m != null);
             System.out.print("email: ");
             String email = br.readLine();
@@ -49,7 +57,7 @@ public class MemberService {
             System.out.print("카테고리: ");
             Long category_id = Long.valueOf(br.readLine());
 
-            dao.insert(new Member(username, password, nickname, email, category_id));
+            mdao.insert(new Member(username, password, nickname, email, category_id));
             tamagotchiDao.insert(
                     Tamagotchi.builder()
                             .id(1) // 사용안함
@@ -71,20 +79,20 @@ public class MemberService {
         System.out.println("=====회원 검색=====");
         System.out.print("검색할 회원 닉네임: ");
         String nickname = br.readLine();
-        System.out.println(dao.select(2, nickname));
+        System.out.println(mdao.select(2, nickname));
     }
 
     // 회원 정보 수정(비밀번호)
     public void updatePwd(BufferedReader br) throws IOException {
         System.out.println("=====비밀번호 변경=====");
         if (loginId != null) {
-            Member m = dao.selectById(loginId);
+            Member m = mdao.selectById(loginId);
             System.out.print("새로운 비밀번호: ");
             String new_pwd = br.readLine();
             System.out.print("비밀번호 확인: ");
             String check_pwd = br.readLine();
             if (check_pwd.equals(new_pwd)) {
-                dao.updatePwd(new_pwd, loginId);
+                mdao.updatePwd(new_pwd, loginId);
             } else {
                 System.out.println("비밀번호가 일치하지 않습니다.");
             }
@@ -97,16 +105,16 @@ public class MemberService {
     public void updateNickname(BufferedReader br) throws IOException {
         System.out.println("=====닉네임 변경=====");
         if (loginId != null) {
-            Member m = dao.selectById(loginId);
+            Member m = mdao.selectById(loginId);
             String nickname = "";
             Member m2 = null;
             do {
                 System.out.print("변경할 닉네임: ");
                 nickname = br.readLine();
-                m2 = dao.select(2, nickname);
+                m2 = mdao.select(2, nickname);
             } while (m2 != null);
 
-            dao.updateNickname(nickname, loginId);
+            mdao.updateNickname(nickname, loginId);
         } else {
             System.out.println("로그인 후 사용해주세요.");
         }
@@ -126,10 +134,10 @@ public class MemberService {
     public void updateCategory(BufferedReader br) throws IOException {
         if (loginId != null) {
             System.out.println("=====카테고리 변경=====");
-            Member m = dao.selectById(loginId);
+            Member m = mdao.selectById(loginId);
             System.out.print("1.학생 2.취준생 3.자격증준비");
             Long c_id = Long.valueOf(br.readLine());
-            dao.updateCategory(loginId, c_id);
+            mdao.updateCategory(loginId, c_id);
         } else {
             System.out.println("로그인 후 사용해주세요.");
         }
@@ -144,7 +152,7 @@ public class MemberService {
             System.out.print("PASSWORD: ");
             String password = br.readLine();
 
-            Member m = dao.select(1, username);
+            Member m = mdao.select(1, username);
             if (password.equals(m.getPassword())) {
                 loginId = m.getId();
                 System.out.println(m.getUsername() + " 로그인 성공");
@@ -166,7 +174,12 @@ public class MemberService {
     public void delMember() {
         System.out.println("=====회원 탈퇴=====");
         if (loginId != null) {
-            dao.delete(loginId);
+            if(gdao.selectAll(loginId).size() != 0){
+                System.out.println("내가 그룹장인 그룹 존재. 탈퇴 불가능");
+            } else {
+                mdao.delete(loginId);
+                gmdao.delete(loginId, GroupService.groupId);
+            }
         } else {
             System.out.println("로그인 후 사용해주세요.");
         }
@@ -175,7 +188,7 @@ public class MemberService {
     public void printMember(){
         System.out.println("=====내 정보 확인=====");
         String title = titleService.getTitle();// 타이틀 가공해주세요.!!
-        Member member = dao.selectById(loginId);
+        Member member = mdao.selectById(loginId);
         System.out.println(title + " " + member.getNickname());
     }
 }
