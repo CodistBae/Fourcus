29da0e045cf487e3409acfa063a7579cf1135b82
Kosta <abdortus159@naver.com>
2023-09-01 10:56:56 +0900

    9/1 GroupMember 추방기능 내 그룹원 확인

--- Fourcus/src/main/java/groupmember/dao/GroupMemberDao.java
+++ Fourcus/src/main/java/groupmember/dao/GroupMemberDao.java
@@ -11,8 +11,8 @@ public class GroupMemberDao {
         dbUtils = DbUtils.getInstance();
     }
 
-    // 그룹장의 기능
-// 멤버 추가 (해당 그룹에 해당 멤버가 없어야함)
+//* 그룹장의 기능 *//
+    // 멤버 추가 (해당 그룹에 해당 멤버가 없어야함)
     public void insert(String username, long Group_id) { // 그룹원의idx, Member_id, Group_id, Cumulative_time
 
         String sql = """
@@ -36,6 +36,7 @@ public class GroupMemberDao {
 
         }
     }
+
     // 그룹멤버 추방 (내 그룹에 있는)
     public void delete(long Member_id, long Group_id) {
 
@@ -56,10 +57,8 @@ public class GroupMemberDao {
     }
 
 
-    //* 공통기능 *//
-    // 나와 같은 Group_id를 가진 그룹원 中 Member_id를 입력해서 GroupMember 테이블 가져오기
+//* 공통기능 *//
     // 전체 검색 (내 그룹원들만) -> 누적시간까지 포함!
-
     public ArrayList<GroupMember> selectAll(long Group_id) {
         ArrayList<GroupMember> list = new ArrayList<>();
 
@@ -87,6 +86,34 @@ public class GroupMemberDao {
         }
         return list;
     }
+    // 나와 같은 Group_id를 가진 그룹원 中 Member_id를 입력해서 멤버의 정보확인
+    public GroupMember selectGroupMember (long Member_id, long Group_id){
+
+        String sql = """
+                select gm.Id, gm.Member_id, gm.Group_id, Cumulative_time
+                from GroupMember gm
+                join `Group` g on g.id = gm.Group_id
+                where gm.Member_id =? and gm.Group_id =?
+                """;
+        try (Connection connection = dbUtils.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, Member_id);
+            pstmt.setLong(2, Group_id);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new GroupMember(
+                        rs.getLong(1),
+                        rs.getLong(2),
+                        rs.getLong(3),
+                        rs.getLong(4)
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 
     // 내 그룹원인지 확인( Member_id -> Group_id)
     public boolean checkMyGroup(long Member_id, long Group_id) {
@@ -111,29 +138,4 @@ public class GroupMemberDao {
             throw new RuntimeException(e);
         }
     }
-//// 그룹의 누적시간 가져오기 ?
-//    public ArrayList<Long> selectCumTime(){
-//        ArrayList<Long> list = new ArrayList<>();
-//        String sql = " select a.Cumulative_time from GroupMember g \n" +
-//                "join `Subject` s on g.Member_id = s.member_id\n" +
-//                "join StudyHour a on s.id = a.subject_id\n" +
-//                "where g.Group_id = ?";
-//
-//        try(Connection connection =dbUtils.getConnection();
-//            PreparedStatement pstmt = connection.prepareStatement(sql)){
-//
-//            ResultSet rs = pstmt.executeQuery();
-//            while(rs.next()){
-//                list.add(new GroupMember(
-//                        rs.getLong(1), // Id
-//                        rs.getLong(2), // Member_id
-//                        rs.getLong(3), // Group_name
-//                        rs.getLong(4) // Cumulative_time
-//                ));
-//            }
-//        } catch(SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return list;
-//    }
 }
